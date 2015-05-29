0f3f625c43811b93f8ef6bb9a91b5a4781f53739
Calvin Metcalf <cmetcalf@appgeo.com>
2015-05-29 08:12:02 -0400

    use length

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 'use strict';
 var https = require('https');
+var http = require('http');
 var stream = require('io-stream');
 var url = require('url');
 var qs = require('querystring');
@@ -44,11 +45,12 @@ function getUploadState(credentials, destination, callback) {
 }
 function noop(){}
 function cartodbUploader(credentials, fileName, callback) {
+  debug('starting upload');
   var compress = false;
-  if (['zip', '.gz', 'bz2', 'tgz', 'kmz', 'lsx', 'ods'].indexOf(fileName.slice(-3)) === -1) {
-    fileName = fileName += '.gz';
-    compress = true;
-  }
+  // if (['zip', '.gz', 'bz2', 'tgz', 'kmz', 'lsx', 'ods'].indexOf(fileName.slice(-3)) === -1) {
+  //   fileName = fileName += '.gz';
+  //   compress = true;
+  // }
   callback = once(callback || noop);
   var passthrough = compress ? zlib.createGzip() : new stream.PassThrough();
   passthrough.on('error', callback);
@@ -63,45 +65,63 @@ function cartodbUploader(credentials, fileName, callback) {
   fullUrl += qs.stringify({
       api_key: credentials.key
   });
-  var parsedUrl = url.parse(fullUrl);
-  parsedUrl.method = 'POST';
-  parsedUrl.headers = form.getHeaders();
-  debug(parsedUrl);
-  var req = https.request(parsedUrl, function (res) {
-    var data = [];
-    debug(res.statusCode);
-    res
-      .on('error', callback)
-      .on('data', function (d){
-        debug(d.toString());
-        data.push(d);
-      })
-      .on('end', function (){
-        var out = Buffer.concat(data).toString();
-        if (res.statusCode > 299) {
-          callback(out);
-        } else {
-          try {
-            out = JSON.parse(out);
-          } catch(e) {
-            return callback(e);
-          }
-          if (!out.success) {
-            return callback(new Error('failure'));
-          }
-          getUploadState(credentials, out.item_queue_id, callback);
-        }
-      });
-  }).on('error', callback);
-  form.pipe(req);
+
+
+    var datas = [];
+    var len = 0;
+    form.pipe(new stream.Transform({
+      transform: function (chunk, _, next){
+        datas.push(chunk);
+        len += chunk.length;
+        next();
+      },
+      flush: function (done) {
+        var parsedUrl = url.parse(fullUrl);
+        parsedUrl.method = 'POST';
+        parsedUrl.headers = form.getHeaders({
+          'content-length': len
+        });
+        debug(parsedUrl);
+        var req = https.request(parsedUrl, function (res) {
+          var data = [];
+          debug(res.statusCode);
+          res
+            .on('error', callback)
+            .on('data', function (d){
+              debug(d.toString());
+              data.push(d);
+            })
+            .on('end', function (){
+              var out = Buffer.concat(data).toString().trim();
+              if (res.statusCode > 299) {
+                callback(out || new Error(http.STATUS_CODES[res.statusCode]));
+              } else {
+                try {
+                  out = JSON.parse(out);
+                } catch(e) {
+                  return callback(e);
+                }
+                if (!out.success) {
+                  return callback(new Error('failure'));
+                }
+                getUploadState(credentials, out.item_queue_id, callback);
+              }
+            });
+        }).on('error', callback);
+        datas.forEach(function (item) {
+          req.write(item);
+        });
+        req.end();
+      }
+    }));
   return passthrough;
 }
 exports.geojson = uploadGeoJson;
 function uploadGeoJson(credentials, destination, callback) {
+  debug('starting geojson');
   callback = once(callback);
   var inStream = geojsonStream.stringify();
-  inStream.on('error', callback);
-  inStream
-    .pipe(cartodbUploader(credentials, destination + '.geojson.gz', callback));
+  inStream.on('error', callback)
+    .pipe(cartodbUploader(credentials, destination + '.geojson', callback));
   return inStream;
 }
